62c30307dd0666c9010c6a2324f4c5354e170aad
mun <git@t3hmun.com>
2019-03-15 07:15:26 +0000

    Cover few more cases, gestalt passes.

--- gestalt_test.go
+++ gestalt_test.go
@@ -13,6 +13,9 @@ var gestalttests = []struct {
 	{"Odd len even separator", []rune("abcdefghijk"), 7, []rune("--"), "ab--jk"},
 	{"Even len odd separator", []rune("abcdefghijk"), 8, []rune("-"), "abc-ijk"},
 	{"Even len even separator", []rune("abcdefghijk"), 8, []rune("--"), "abc--ijk"},
+	{"Text shorter than maxlen", []rune("abcdefghijk"), 20, []rune("--"), "abcdefghijk"},
+	{"Text same len as maxlen", []rune("abcdefghijk"), 11, []rune("--"), "abcdefghijk"},
+	{"Text one longer than maxlen", []rune("abcdefghijk"), 10, []rune("--"), "abcd--hijk"},
 }
 
 func TestGestalt(t *testing.T) {
